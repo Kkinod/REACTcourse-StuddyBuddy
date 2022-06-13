a40817a2344de64fdfe6b47f818b6ad13898559f
unknown <kamil.p@gnasze-wlasne.pl>
2022-06-13 16:43:50 +0200

    add handler for btn component

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types'
 import Button from 'components/atoms/Button/Button'
 import { Wrapper, StyledAverage, StyledInfo } from './UsersListItem.styles'
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
+const showIndex = index => alert(`This is student #${index + 1}`)
+
+const UsersListItem = ({ index, userData: { average, name, attendance = '0%' } }) => (
 	<Wrapper>
-	  <StyledAverage value={average}>{average}</StyledAverage>
-	  <StyledInfo>
-		<p>
-		  {name}
-		  <Button />
-		</p>
-		<p>attendance: {attendance}</p>
-	  </StyledInfo>
+		<StyledAverage value={average}>{average}</StyledAverage>
+		<StyledInfo>
+			<p>
+				{name}
+				<Button onClick={() => showIndex(index)} />
+			</p>
+			<p>attendance: {attendance}</p>
+		</StyledInfo>
 	</Wrapper>
-  );
+)
 
 UsersListItem.propTypes = {
 	userData: PropTypes.shape({
